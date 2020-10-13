1ca47b1e12c53816abfc2721ceefcabc907fa2dd
zkan <zkan.cs@gmail.com>
2020-10-13 21:28:04 +0700

    :white_check_mark: Use fixtures in tests :bear:

--- testing_django/categories/tests/test_models.py
+++ testing_django/categories/tests/test_models.py
@@ -8,6 +8,8 @@ from ..models import Category
 
 
 class TestCategory(TestCase):
+    fixtures = ['categories',]
+
     def test_catgory_should_have_defined_fields(self):
         # Given
         name = 'Personal'
@@ -22,3 +24,13 @@ class TestCategory(TestCase):
         self.assertEqual(category.photo.name, photo_mock.name)
 
         os.remove(f'media/{photo_mock.name}')
+
+    def test_catgory_fixtures(self):
+        categories = Category.objects.all()
+        expected_results = (
+            ('Street', 'logo-android.png'),
+            ('Italian', 'logo-kotlin.png'),
+        )
+        for category, (expected_name, expected_photo_name) in zip(categories, expected_results):
+            assert category.name == expected_name
+            assert category.photo.name == expected_photo_name
